da79f34755d1f7ed57a9d25f0498d76dfd9a07af
LuisXalcedo <luis.e.ss@gmail.com>
2023-07-02 09:00:18 -0600

    complete react mock code challenge

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ListingCard from "./ListingCard";
 // import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({ searchInput }) {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+        // console.log(data);
+      });
+  }, []);
+
+  function handleDelete(id) {
+    const deletedProduct = products.filter((product) => {
+      return product.id !== id;
+    });
+
+    setProducts(deletedProduct);
+  }
+
+  const filteredSearchProduct = products.filter((product) => {
+    return product.description
+      .toLowerCase()
+      .includes(searchInput.toLowerCase());
+  });
+
+  const productsToDisplay = filteredSearchProduct.map((product) => {
+    return (
+      <ListingCard
+        key={product.id}
+        product={product}
+        handleDelete={handleDelete}
+      />
+    );
+  });
   return (
     <main>
-      <ul className="cards">
-        {/* use the ListingCard component to display listings */}
-      </ul>
+      <ul className="cards">{productsToDisplay}</ul>
     </main>
   );
 }
